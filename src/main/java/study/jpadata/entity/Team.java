85b210adac78c7e718de55b22c95df8ffcf133fb
Brian <42661845+hojoungjang@users.noreply.github.com>
2025-02-26 17:49:09 +0900

    예제 도메인 모델 추가하기 (#1)

---
+++ src/main/java/study/jpadata/entity/Team.java
@@ -0,0 +1,33 @@
+package study.jpadata.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString(of = {"id", "name"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+    
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
