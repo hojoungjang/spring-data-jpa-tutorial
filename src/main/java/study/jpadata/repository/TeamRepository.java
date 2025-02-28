e917a8af4ea25afb31aa2e20e9baff1770499439
Brian <42661845+hojoungjang@users.noreply.github.com>
2025-02-28 10:01:23 +0900

    공통 인터페이스 기능 살펴보기 (#2)
    * Add pure JPA repository classes
    
    * Add JpaRepository interfaces and tests

---
+++ src/main/java/study/jpadata/repository/TeamRepository.java
@@ -0,0 +1,9 @@
+package study.jpadata.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import study.jpadata.entity.Team;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+    
+}
